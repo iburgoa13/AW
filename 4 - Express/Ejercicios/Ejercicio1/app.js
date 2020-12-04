a3ecc2ee72983c262d87ca141af567c227747431
iburgoa <iburgoa@ucm.es>
2020-12-04 11:50:51 +0100

    Ejercicio 0 de los ejercicios express hecho

---
+++ 4 - Express/Ejercicios/Ejercicio1/app.js
@@ -0,0 +1,36 @@
+"use strict"
+
+const path = require("path");
+const Express = require("express");
+const app = Express();
+
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+
+
+
+const usuarios = [
+    {nombre: "Iker Burgoa" , numero: 123456},
+    {nombre: "Morgan Freeman" , numero: 654321},
+    {nombre: "Daniela" , numero: 182937},
+    {nombre: "Maradona" , numero: 938271}
+]
+
+
+
+app.listen(3000, (err) => {
+    if(err) console.log(err.message);
+    else console.log("Escuchando en el puerto 3000");
+});
+
+app.get("/usuarios", function(request, response){
+    response.redirect("/users");
+});
+
+app.get("/users", function(request, response){
+    response.render("usuarios",  {users: usuarios} );
+});
+
+app.get("/socios", function(request, response) {
+    response.redirect("/users");
+});
