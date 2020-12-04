5e6838ddd9d00d5e51b63645a259fbdc782280a4
iburgoa <iburgoa@ucm.es>
2020-12-04 10:35:02 +0100

    Hecho bien lo de ayer, empezado primer ejercicio , terminarlo cuando se pueda

--- 4 - Express/app.js
+++ 4 - Express/app.js
@@ -2,15 +2,17 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+var usuarios =["Pepe Villuela","Dolores Alsonso","Jose luis"];
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 app.get("/", function(request, response) {
 response.status(200);
 response.type("text/plain; charset=utf-8");
 response.end("Esta es la página raíz");
 });
 app.get("/users.html", function(request, response) {
-response.status(200);
-response.type("text/plain; charset=utf-8");
-response.end("Aquí se mostrará la página de usuarios");
+    response.status(200);
+    response.render("users",{users:usuarios});
 });
 app.listen(3000, function(err) {
 if (err) {
