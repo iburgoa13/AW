5fc4fffa2a3d872034f5990f25d3636a3f48390f
iburgoa <iburgoa@ucm.es>
2020-12-22 18:03:06 +0100

    Practica 5 con el sql

--- Practicas/Practicas Voluntarias/Practica4/script_sql/tareas.sql
+++ Practicas/Practicas Voluntarias/Practica4/script_sql/tareas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-12-2020 a las 00:40:52
+-- Tiempo de generación: 22-12-2020 a las 18:02:05
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.2
 
@@ -25,6 +25,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) UNSIGNED NOT NULL,
+  `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `tag`
 --
 
@@ -38,10 +50,10 @@ CREATE TABLE `tag` (
 --
 
 INSERT INTO `tag` (`taskId`, `tag`) VALUES
-(1, 'AW'),
-(1, 'UCM'),
-(2, 'Academia'),
-(2, 'MS');
+(1, 'pe'),
+(1, 'ww'),
+(2, 'dede'),
+(3, 'ee');
 
 -- --------------------------------------------------------
 
@@ -55,14 +67,6 @@ CREATE TABLE `task` (
   `text` text COLLATE utf8_spanish2_ci NOT NULL,
   `done` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `task`
---
-
-INSERT INTO `task` (`id`, `user`, `text`, `done`) VALUES
-(1, 'usuario@ucm.es', 'Examen AW', 0),
-(2, 'usuario@ucm.es', 'Clase Academia MS', 0);
 
 -- --------------------------------------------------------
 
@@ -81,11 +85,18 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`email`, `password`, `img`) VALUES
+('pepe@pepe.es', '1234', 'image1.jpg'),
 ('usuario@ucm.es', '1234', '');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`session_id`);
 
 --
 -- Indices de la tabla `tag`
@@ -114,7 +125,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `task`
 --
 ALTER TABLE `task`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
